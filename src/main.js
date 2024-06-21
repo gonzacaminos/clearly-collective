10ce23eab5b6cec7aa48affab8b3137997169670
gonzacaminos <gonza@gonza.io>
2024-06-20 19:23:09 -0500

    How to wear section

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  */
 import "./scss/main.scss";
 import Alpine from "alpinejs";
-import Splide from '@splidejs/splide';
+import Splide from "@splidejs/splide";
 
 window.Alpine = Alpine;
 
@@ -33,14 +33,17 @@ if (sort_by) {
   });
 }
 
-const carousels = document.querySelectorAll( '.splide-slider' );
+const carousels = document.querySelectorAll(".splide-slider");
 
-carousels.forEach(carousel => {
+carousels.forEach((carousel) => {
   const data = carousel.dataset;
-  new Splide( carousel, {
+  new Splide(carousel, {
     perPage: data?.perPage,
-    padding: { left: parseInt(data?.paddingLeft || 0) , right: parseInt(data?.paddingRight || 0) },
-    gap: data?.gap || '24px',
+    padding: {
+      left: parseInt(data?.paddingLeft || 0),
+      right: parseInt(data?.paddingRight || 0),
+    },
+    gap: data?.gap || "24px",
     pagination: false,
     breakpoints: {
       480: {
@@ -48,45 +51,81 @@ carousels.forEach(carousel => {
       },
       640: {
         perPage: 1,
-        padding: 10
+        padding: 10,
       },
       980: {
-        perPage: 2
-      }
-    }
+        perPage: 2,
+      },
+    },
   }).mount();
 });
 
+const vertical_carousel = document.querySelector(
+  ".collection-link-blocks .splide-slider-vertical"
+);
 
-
-const vertical_carousel = document.querySelector( '.collection-link-blocks .splide-slider-vertical' );
-
-if(vertical_carousel){
-  const collection_link_block = new Splide( vertical_carousel, {
+if (vertical_carousel) {
+  const collection_link_block = new Splide(vertical_carousel, {
     perPage: 1,
     arrows: false,
-    direction: 'ttb',
+    direction: "ttb",
     heightRatio: 1,
-    speed: 600
+    speed: 500,
+    focus: 0,
   }).mount();
-  
-  const collection_links = document.querySelectorAll('.collection-link-blocks a.tw-group');
-  
-  collection_links.forEach(collection_link => {
+
+  const collection_links = document.querySelectorAll(
+    ".collection-link-blocks a.tw-group"
+  );
+
+  collection_links.forEach((collection_link) => {
     collection_link.addEventListener("mouseenter", (event) => {
       const index = parseInt(event.currentTarget.dataset?.index);
-      console.log(index)
-      collection_link_block.go(index)
+      console.log(index);
+      collection_link_block.go(index);
     });
   });
 
-  const interaction_container = document.querySelector( '.collection-link-blocks .interaction-container' );
+  const interaction_container = document.querySelector(
+    ".collection-link-blocks .interaction-container"
+  );
 
-  if(interaction_container){
+  if (interaction_container) {
     interaction_container.addEventListener("mouseleave", (event) => {
-      collection_link_block.go(0)
+      collection_link_block.go(0);
     });
   }
+}
+
+const how_to_wear = document.querySelector(".splide-how-to-wear");
+const how_to_wear_text = document.querySelector(".splide-how-to-wear-text");
+
+if (how_to_wear && how_to_wear_text) {
+  const how_to_wear_slider = new Splide(how_to_wear, {
+    perPage: 1,
+    arrows: false,
+    padding: { right: 50 },
+    breakpoints: {
+      989: {
+        padding: 0,
+      },
+    },
+    gap: "16px",
+    speed: 600,
+  }).mount();
+
+  const how_to_wear_text_slider = new Splide(how_to_wear_text, {
+    arrows: false,
+    speed: 400,
+    type: "fade",
+    drag: false,
+    rewind: true,
+  }).mount();
+
+  how_to_wear_slider.on("move", (e) => {
+    console.log(e);
+    how_to_wear_text_slider.go(e);
+  });
 }
 
 // new Ajaxinate({
