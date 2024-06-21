74f59e39c6f85582fad3b57256687ccf655f6c94
gonzacaminos <gonza@gonza.io>
2024-06-21 16:55:25 -0500

    Replaced spacing scale

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,6 +8,24 @@ const path = require("path");
 
 module.exports = {
   theme: {
+    spacing: {
+      0: "0px",
+      1: "8px",
+      1.5: "12px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+      10: "80px",
+      11: "88px",
+      12: "96px",
+      13: "104px",
+      14: "112px",
+    },
     extend: {
       screens: {
         mobile: "480px",
